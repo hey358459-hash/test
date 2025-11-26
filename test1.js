f14846cfb3b04189a42edf597b3359186845bbe7
hey358459-hash <hey358459@gmail.com>
2025-11-26 15:34:09 +0100

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,8 @@
+(function() {
+function printHelloWorld()
+{
+    console.log("Hello world from script hosted on
+    	github repo and deliverd through jsDelivr");
+    alert("XSS comfirmed")
+}
+})();
